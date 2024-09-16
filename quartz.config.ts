4feb5086e7c92989846d171376fcaab6cb954b1e
meydanoglu <eminmeydanoglu@gmail.com>
2024-09-16 13:38:06 +0300

    Quartz sync: Sep 16, 2024, 1:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "published",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
@@ -32,8 +32,8 @@ const config: QuartzConfig = {
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#000000",
-          //#2b2b2b
+          dark: "#2b2b2b",
+          //
           secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
