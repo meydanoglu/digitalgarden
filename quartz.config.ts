7331ffe273259a3943461fd5b55e39261f415e13
meydanoglu <eminmeydanoglu@gmail.com>
2024-09-16 11:47:56 +0300

    Quartz sync: Sep 16, 2024, 11:47 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Emin Meydanoğlu",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -32,7 +32,8 @@ const config: QuartzConfig = {
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
+          dark: "#000000",
+          //#2b2b2b
           secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
